541c73d3b42090c347c4ac48f267157f191e71f3
leidosdaren <114755137+leidosdaren@users.noreply.github.com>
2022-10-01 08:23:27 -0400

    3 files modified

--- src/app/camundaform/camundaform.component.ts
+++ src/app/camundaform/camundaform.component.ts
@@ -17,15 +17,20 @@ const formData =
 */
 
 type CamundaFormData = {
+  form: {
     id: String;
     processDefinitionId: String;
     schema: String;
   };
+};
 
-
+/*
 const formData =
-  '{"id": "id", "processDefinitionId": "PDI", "schema": "schema"}';
-
+  '{"form": {"id": "userTaskForm_2r4pvtj", "processDefinitionId": "2251799813734394", "schema": { "components": [ { "label": "Zipcode", "type": "textfield", "id": "Field_0hwzx7e", "key": "field_zipcode", "description": "Supply a zipcode", "validate": { "required": true, "minLength": 5, "maxLength": 5} } ], "schemaVersion": 4, "exporter": { "name": "Camunda Web Modeler", "version": "fa04aaa" }, "type": "default","id": "Form_6cfd03a7-9542-4f94-84a3-dc0800490846", "executionPlatform": "Camunda Cloud", "executionPlatformVersion": "1.3" }}}';
+*/
+const formData =
+  '{"form": {"id": "userTaskForm_2r4pvtj", "processDefinitionId": "2251799813734394", "schema": "schema" }}';
+  
 @Component({
   selector: 'camundaform',
   templateUrl: './camundaform.component.html',
