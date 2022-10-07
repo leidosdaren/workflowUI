08a55f765888173f8d8ff38aa47437f84d898783
daren arnold <daren.arnold@leidos.com>
2022-10-06 20:15:21 -0400

    Works (mostly)

--- src/app/camundaform/camundaform.component.ts
+++ src/app/camundaform/camundaform.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CamundaService } from '../camunda.service';
+import { FormQuery } from 'src/graphql/generated';
+import { Observable } from 'rxjs';
 /*
 export interface CamundaFormData {
   data: {
@@ -16,13 +18,9 @@ const formData =
   '{"data": { "form": { "id": "userTaskForm_2r4pvtj","processDefinitionId": "2251799813734394", "schema": "{\n  "components": [\n    {\n      "label": "Zipcode",\n      "type": "textfield",\n      "id": "Field_0hwzx7e",\n      "key": "field_zipcode",\n      "description": "Supply a zipcode",\n      "validate": {\n        "required": true,\n        "minLength": 5,\n        "maxLength": 5\n      }\n    }\n  ],\n  "schemaVersion": 4,\n  "exporter": {\n    "name": "Camunda Web Modeler",\n    "version": "fa04aaa"\n  },\n  "type": "default",\n  "id": "Form_6cfd03a7-9542-4f94-84a3-dc0800490846",\n  "executionPlatform": "Camunda Cloud",\n  "executionPlatformVersion": "1.3"\n}" } } }';
 */
 
-type CamundaFormData = {
-  form: {
-    id: String;
-    processDefinitionId: String;
-    schema: String;
-  };
-};
+const id = "userTaskForm_2r4pvtj";
+const processDefinitionId = "2251799813734394";
+
 
 /*
 const formData =
@@ -35,11 +33,25 @@ const formData =
   selector: 'camundaform',
   templateUrl: './camundaform.component.html',
 })
-export class CamundaFormComponent implements OnInit {
-  camundaFormJSON: CamundaFormData;
 
+export class CamundaFormComponent implements OnInit {
+  
+  constructor(private camundaService: CamundaService) {}
+
+  schema: any;
+  camundaForm: Observable<FormQuery['form']>;
+  components: any;
+  jsonComponents: any;
+
+  getSchema(schema: any) {
+    console.log("in getSchema: " + schema);
+    this.schema = schema;
+    this.jsonComponents = JSON.parse(schema);
+  }
   ngOnInit() {
-    console.log(formData);
-    this.camundaFormJSON = JSON.parse(formData);
+    
+    this.camundaForm = this.camundaService.getForm(id, processDefinitionId);
+    this.camundaService.getForm(id, processDefinitionId).subscribe(form => this.getSchema(form.schema));
+   
   }
 }
