3f8706f67eeeca52318d7c1ff5c9175dfe67e8d9
daren arnold <daren.arnold@leidos.com>
2022-10-11 14:59:25 -0400

    updates

--- src/app/camundaform/camundaform.component.ts
+++ src/app/camundaform/camundaform.component.ts
@@ -2,42 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { CamundaService } from '../camunda.service';
 import { FormQuery } from 'src/graphql/generated';
 import { Observable } from 'rxjs';
-/*
-export interface CamundaFormData {
-  data: {
-    form: {
-      id: String;
-      processDefinitionId: String;
-      schema: String;
-    };
-  };
-}
-*/
-/*
-const formData =
-  '{"data": { "form": { "id": "userTaskForm_2r4pvtj","processDefinitionId": "2251799813734394", "schema": "{\n  "components": [\n    {\n      "label": "Zipcode",\n      "type": "textfield",\n      "id": "Field_0hwzx7e",\n      "key": "field_zipcode",\n      "description": "Supply a zipcode",\n      "validate": {\n        "required": true,\n        "minLength": 5,\n        "maxLength": 5\n      }\n    }\n  ],\n  "schemaVersion": 4,\n  "exporter": {\n    "name": "Camunda Web Modeler",\n    "version": "fa04aaa"\n  },\n  "type": "default",\n  "id": "Form_6cfd03a7-9542-4f94-84a3-dc0800490846",\n  "executionPlatform": "Camunda Cloud",\n  "executionPlatformVersion": "1.3"\n}" } } }';
-*/
+import {FormGroup} from '@angular/forms';
+import {FormlyFieldConfig, FormlyFormOptions} from '@ngx-formly/core';
+import { FieldType } from '@ngx-formly/material';
 
 const id = "userTaskForm_2r4pvtj";
 const processDefinitionId = "2251799813734394";
-
-
-/*
-const formData =
-  '{"form": {"id": "userTaskForm_2r4pvtj", "processDefinitionId": "2251799813734394", "schema": { "components": [ { "label": "Zipcode", "type": "textfield", "id": "Field_0hwzx7e", "key": "field_zipcode", "description": "Supply a zipcode", "validate": { "required": true, "minLength": 5, "maxLength": 5} } ], "schemaVersion": 4, "exporter": { "name": "Camunda Web Modeler", "version": "fa04aaa" }, "type": "default","id": "Form_6cfd03a7-9542-4f94-84a3-dc0800490846", "executionPlatform": "Camunda Cloud", "executionPlatformVersion": "1.3" }}}';
-*/
-const formData =
-  '{"form": {"id": "userTaskForm_2r4pvtj", "processDefinitionId": "2251799813734394", "schema": "schema" }}';
   
 @Component({
   selector: 'camundaform',
+
   templateUrl: './camundaform.component.html',
+  styleUrls: ['../styles.scss']
 })
 
 export class CamundaFormComponent implements OnInit {
   
   constructor(private camundaService: CamundaService) {}
-
+  form: FormGroup;
+  model: Object;
+  fields: FormlyFieldConfig[];
+  options: FormlyFormOptions;
   schema: any;
   camundaForm: Observable<FormQuery['form']>;
   components: any;
@@ -53,5 +38,24 @@ export class CamundaFormComponent implements OnInit {
     this.camundaForm = this.camundaService.getForm(id, processDefinitionId);
     this.camundaService.getForm(id, processDefinitionId).subscribe(form => this.getSchema(form.schema));
    
+    this.form = new FormGroup({});
+    this.model = { email: 'email@gmail.com' };
+    this.options = {};
+  this.fields  = [
+    {
+      key: 'email',
+      type: 'input',
+      templateOptions: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true,
+      }
+    },
+  ];
+  }
+  onSubmit(model: Object) {
+    alert(
+      JSON.stringify(this.model, null, 4)
+    );
   }
 }
